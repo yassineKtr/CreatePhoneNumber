c6a96621cf7292dc61bf239bfd5ee3eb0b270ae2
yassine ktiri <yassinektiri@gmail.com>
2022-06-01 00:16:59 +0100

    initial commit

---
+++ CreatePhoneNumber.Tests/KataTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace CreatePhoneNumber.Tests
+{
+    public class KataTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal("(123) 456-7890", Kata.CreatePhoneNumber(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }));
+        }
+    }
+}
+}
---
+++ CreatePhoneNumber/Kata.cs
@@ -0,0 +1,10 @@
+﻿namespace CreatePhoneNumber
+{
+    public class Kata
+    {
+        public static string CreatePhoneNumber(int[] num)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
