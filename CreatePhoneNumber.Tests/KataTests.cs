ccba7a7eb78712bd1e314e07e69551b1eb7fc3e6
yassine ktiri <yassinektiri@gmail.com>
2022-06-03 01:55:16 +0100

    Fixed the issues mentioned in the review

--- CreatePhoneNumber.Tests/KataTests.cs
+++ CreatePhoneNumber.Tests/KataTests.cs
@@ -5,7 +5,7 @@ namespace CreatePhoneNumber.Tests
     public class KataTests
     {
         [Fact]
-        public void Test1()
+        public void ShouldCreatePhoneNumber()
         {
             Assert.Equal("(123) 456-7890", Kata.CreatePhoneNumber(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }));
         }
--- CreatePhoneNumber/Kata.cs
+++ CreatePhoneNumber/Kata.cs
@@ -5,11 +5,7 @@
         public static string CreatePhoneNumber(int[] num)
         {
             if (num.Length != 10) return "";
-            var firstPart = "(" + string.Join("", num.Take(3)) + ")" + " ";
-            var secondPart = string.Join("", num.Skip(3).Take(3));
-            var thirdPart = string.Join("", num.Skip(6));
-            var lastPart = string.Join("-", secondPart, thirdPart);
-            return firstPart + lastPart;
+            return $"({string.Join("", num.Take(3))}) {string.Join("", num.Skip(3).Take(3))}-{string.Join("", num.Skip(6))}";
         }
     }
 }
