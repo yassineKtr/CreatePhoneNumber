c994f78bf98d1639587050f73c7a0c2002ca00a3
yassine ktiri <yassinektiri@gmail.com>
2022-06-06 22:11:07 +0100

    Fixed last mentioned issues

--- CreatePhoneNumber/Kata.cs
+++ CreatePhoneNumber/Kata.cs
@@ -4,7 +4,7 @@
     {
         public static string CreatePhoneNumber(int[] num)
         {
-            return num.Length != 10 ? "":
+            return num.Length != 10 ? String.Empty:
             $"({string.Join("", num.Take(3))}) {string.Join("", num.Skip(3).Take(3))}-{string.Join("", num.TakeLast(4))}";
         }
     }
